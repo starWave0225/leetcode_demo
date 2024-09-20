38f85ec1c2507ea98e72e15f9e176e3a265b3108
starWave0225 <junjiequ98@gmail.com>
2024-09-20 20:51:08 +0800

    241

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -1880,4 +1880,28 @@ public class Solution {
         return prev;
     }
 
+    // 241. Different Ways to Add Parentheses
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '+' || ch == '-' || ch == '*') {
+                List<Integer> s1 = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (ch == '+')
+                            res.add(a + b);
+                        else if (ch == '-')
+                            res.add(a - b);
+                        else if (ch == '*')
+                            res.add(a * b);
+                    }
+                }
+            }
+        }
+        if (res.isEmpty())
+            res.add(Integer.parseInt(expression));
+        return res;
+    }
 }
