0f05c96598e2bb6c4b5d651da1051d1a2646161c
starWave0225 <junjiequ98@gmail.com>
2024-10-06 22:02:13 +0800

    1813

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2273,4 +2273,24 @@ public class Solution {
         }
         return false;
     }
+
+    // 1813. Sentence Similarity III
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+        if(s1.length < s2.length){
+            String[] temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+        int start = 0, end = 0;
+        int n1 = s1.length, n2 = s2.length;
+        while(start < n2 && s1[start].equals(s2[start])){
+            start++;
+        }
+        while(end < n2 && s1[n1-end-1].equals(s2[n2-end-1])){
+            end++;
+        }
+        return start + end >= n2;
+    }
 }
