8735934b74154b4dc17cbfe8360f1e014deec750
starWave0225 <junjiequ98@gmail.com>
2024-10-07 20:23:05 +0800

    2696

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2329,7 +2329,27 @@ public class Solution {
 
     // 2696. Minimum String Length After Removing Substrings
     public int minLength(String s) {
-        
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(cur);
+                continue;
+            }
+      
+            if (cur == 'B' && stack.peek() == 'A') {
+                stack.pop();
+            }
+            else if (cur == 'D' && stack.peek() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur);
+            }
+        }
+        return stack.size();
     }
 
 }
