a3839bb159a77a85d60f8b53b51a0d2053ccdbd4
starWave0225 <junjiequ98@gmail.com>
2024-10-11 00:30:36 +0800

    921 962

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2362,4 +2362,44 @@ public class Solution {
         }
         return (size + 1) / 2;
     }
+
+    // 921. Minimum Add to Make Parentheses Valid
+    public int minAddToMakeValid(String s) {
+        int open = 0, close = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0)
+                    open--;
+                else
+                    close++;
+            }
+        }
+        return open + close;
+    }
+
+    // 962. Maximum Width Ramp
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+
+        // Step 1: Build a decreasing stack of indices
+        for (int i = 0; i < n; ++i) {
+            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+
+        int maxWidth = 0;
+
+        // Step 2: Traverse from the end and find maximum width ramp
+        for (int j = n - 1; j >= 0; --j) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
+                maxWidth = Math.max(maxWidth, j - stack.pop());
+            }
+        }
+
+        return maxWidth;
+    }
 }
