ef5e3b57b650f1c131d7311d12441c913da04ae2
starWave0225 <junjiequ98@gmail.com>
2024-10-24 20:15:27 +0800

    update 951

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2683,7 +2683,7 @@ public class Solution {
         if (root1.val != root2.val) {
             return false;
         }
-        if ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
+        else if ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
                 || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))) {
             return true;
         }
