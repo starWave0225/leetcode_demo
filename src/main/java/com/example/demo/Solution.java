2ac2e97b2d9d573b6b8cb1d41e96560ca3d5a599
junjie.qu <junjie.qu@antgroup.com>
2024-10-25 14:04:32 +0800

    2319

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2750,4 +2750,24 @@ public class Solution {
         }
         return res;
     }
+
+    // 2319. Check if Matrix Is X-Matrix
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i == n - 1 - j) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (grid[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
+
