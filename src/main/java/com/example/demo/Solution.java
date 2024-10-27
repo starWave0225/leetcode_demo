a325ffeede997e3231fad3e0f7e293ab74b124fe
starWave0225 <junjiequ98@gmail.com>
2024-10-27 20:22:45 +0800

    1277

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2689,4 +2689,41 @@ public class Solution {
         }
         return false;
     }
+
+    // 1277. Count Square Submatrices with All Ones
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        
+        int ans = 0;
+        
+        // Initialize first column of DP table
+        for (int i = 0; i < m; i++) {
+            if(matrix[i][0] == 1){
+                dp[i][0] = 1;
+                ans++;
+            }
+        }
+        
+        // Initialize first row of DP table
+        for (int j = 1; j < n; j++) {
+            if(matrix[0][j] == 1){
+                dp[0][j] = 1;
+                ans++;
+            }
+        }
+        
+        // Fill the DP table for remaining cells
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 1) {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+                }
+                ans += dp[i][j];
+            }
+        }
+        
+        return ans;
+    }
 }
