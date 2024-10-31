13e6270a93afe9ad1f3d91f85b337bea5ad1218c
starWave0225 <junjiequ98@gmail.com>
2024-10-31 21:36:01 +0800

    1671

--- src/main/java/com/example/demo/Solution.java
+++ src/main/java/com/example/demo/Solution.java
@@ -2806,4 +2806,40 @@ public class Solution {
         }
         return true;
     }
+
+    // 1671. Minimum Number of Removals to Make Mountain Array
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n], right = new int[n];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    left[i] = Math.max(left[i], left[j] + 1);
+                }
+            }
+        }
+
+        // Compute LDS from each index
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[i] > nums[j]) {
+                    right[i] = Math.max(right[i], right[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+
+        // Find the maximum mountain length
+        for (int i = 1; i < n - 1; ++i) {
+            if (left[i] > 1 && right[i] > 1) {  // Valid peak
+                res = Math.max(res, left[i] + right[i] - 1);
+            }
+        }
+
+        return n - res;
+    }
 }
